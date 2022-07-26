11cddb31df9b2f2a1e6181d6fa91190187dc0288
Mahmut DOLAR <109947891+mahmutdolar@users.noreply.github.com>
2022-07-26 10:28:07 +0300

    Update Question1.sql

--- Question1.sql
+++ Question1.sql
@@ -1,8 +1,7 @@
 with
 v_emp as (select first_name,last_name,department_id,salary from hr.employees emp),
-v_dept as (select department_id,max(salary) as max_sal from hr.employees group by department_id)
+v_dept as (select department_id,max(salary) as max_sal from hr.employees where department_id is not null group by department_id)
 select v_emp.first_name,v_emp.last_name,v_dept.max_sal "Ücret"
 from v_emp,v_dept
 where v_emp.department_id(+)=v_dept.department_id and
 v_emp.salary(+)=v_dept.max_sal;
-
