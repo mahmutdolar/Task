6042dca49a04c84d2618e35dfbc27f6b35308c9a
Mahmut DOLAR <109947891+mahmutdolar@users.noreply.github.com>
2022-07-26 10:29:08 +0300

    Update Question3.sql

--- Question3.sql
+++ Question3.sql
@@ -2,7 +2,4 @@ with v_str as (select department_name from hr.departments where manager_id is nu
 select substr(department_name,5,1) from v_str
 
 
-------With olmadan Yazım Tarzı--------
-select substr(department_name,5,1) from hr.departments where manager_id is null 
-
 
