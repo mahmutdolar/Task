928510363d15edffb39771ac108c362746f83b58
Mahmut DOLAR <109947891+mahmutdolar@users.noreply.github.com>
2022-07-26 10:28:50 +0300

    Update Question2.sql

--- Question2.sql
+++ Question2.sql
@@ -5,8 +5,4 @@ select loc_city as "Şehir İsmi",count(*) "Çalışan Sayısı"
 from v_loclist
 group by loc_city 
 
-------With Olmadan Yazım Tarzı------
-select loc.city "Şehir İsmi",count(*) "Çalışan Sayısı"
-from hr.employees emp,hr.departments dept,hr.locations loc
-where emp.department_id=dept.department_id(+) and dept.location_id=loc.location_id(+)
-group by loc.city
+
